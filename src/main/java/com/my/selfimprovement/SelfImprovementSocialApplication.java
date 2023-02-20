5ff07608c2756fab3bbfd304ab6f7eedc065939a
Ruslan Kostiuk <rsln.kostuk@gmail.com>
2023-02-20 16:28:08 +0200

    Init project

---
+++ src/main/java/com/my/selfimprovement/SelfImprovementSocialApplication.java
@@ -0,0 +1,13 @@
+package com.my.selfimprovement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfImprovementSocialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SelfImprovementSocialApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/my/selfimprovement/SelfImprovementSocialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my.selfimprovement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfImprovementSocialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
