edc088748f3d5822ee34ecd01c3d92e2a4f9c4ad
Ruslan Kostiuk <rsln.kostuk@gmail.com>
2023-03-01 13:08:55 +0200

    Add header

--- src/main/resources/static/js/language-select.js
+++ src/main/resources/static/js/language-select.js
@@ -1,6 +1,6 @@
 function handleLanguageSelectChange() {
     // Reload the page with a language parameter set
-    const localeSelect = document.querySelector('#locale-select');
+    const localeSelect = document.querySelector('#localeSelect');
     const parser = new URL(window.location);
     parser.searchParams.set('lang', localeSelect.value);
     window.location = parser.href;
