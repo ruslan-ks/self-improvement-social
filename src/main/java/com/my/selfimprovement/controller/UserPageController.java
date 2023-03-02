5f86412dbb36f67778e4c5eb3f4552648e090b05
Ruslan Kostiuk <rsln.kostuk@gmail.com>
2023-03-02 22:59:41 +0200

    Rename UserPageController

--- src/main/java/com/my/selfimprovement/controller/UserPageController.java
+++ src/main/java/com/my/selfimprovement/controller/UserPageController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/user")
-public class UserController {
+public class UserPageController {
 
     @GetMapping
     @PostMapping
