e29926e983c5d0d7c11e0b569c979017f7545ecd
Ruslan Kostiuk <rsln.kostuk@gmail.com>
2023-04-30 19:55:29 +0300

    Add PropertyReferenceException handling

---
+++ src/main/java/com/my/selfimprovement/controller/advice/PropertyReferenceExceptionAdvice.java
@@ -0,0 +1,22 @@
+package com.my.selfimprovement.controller.advice;
+
+import com.my.selfimprovement.dto.response.ResponseBody;
+import org.springframework.data.mapping.PropertyReferenceException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class PropertyReferenceExceptionAdvice {
+
+    @ExceptionHandler(PropertyReferenceException.class)
+    public ResponseEntity<ResponseBody> handlePropertyReferenceException(PropertyReferenceException ex) {
+        ResponseBody responseBody = ResponseBody.builder()
+                .status(HttpStatus.BAD_REQUEST)
+                .message(ex.getMessage())
+                .build();
+        return ResponseEntity.badRequest().body(responseBody);
+    }
+
+}
