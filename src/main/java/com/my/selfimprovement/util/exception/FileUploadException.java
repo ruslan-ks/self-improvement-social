31cf8e89f26ded2bc6b8634993a36bafa5f8c71c
Ruslan Kostiuk <rsln.kostuk@gmail.com>
2023-05-04 18:17:03 +0300

    Add FileUploadException class

---
+++ src/main/java/com/my/selfimprovement/util/exception/FileUploadException.java
@@ -0,0 +1,20 @@
+package com.my.selfimprovement.util.exception;
+
+public class FileUploadException extends RuntimeException {
+
+    public FileUploadException() {
+        super();
+    }
+
+    public FileUploadException(String message) {
+        super(message);
+    }
+
+    public FileUploadException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public FileUploadException(Throwable cause) {
+        super(cause);
+    }
+}
