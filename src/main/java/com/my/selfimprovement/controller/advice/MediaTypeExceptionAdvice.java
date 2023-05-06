128a64914dfbe6b002ae7cd94937c712fad0228d
Ruslan Kostiuk <rsln.kostuk@gmail.com>
2023-05-06 20:11:44 +0300

    Add org.springframework.http.InvalidMediaTypeException handling to MediaTypeExceptionAdvice

--- src/main/java/com/my/selfimprovement/controller/advice/MediaTypeExceptionAdvice.java
+++ src/main/java/com/my/selfimprovement/controller/advice/MediaTypeExceptionAdvice.java
@@ -3,15 +3,16 @@ package com.my.selfimprovement.controller.advice;
 import com.my.selfimprovement.dto.response.ResponseBody;
 import com.my.selfimprovement.util.exception.IllegalMediaTypeException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.InvalidMediaTypeException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
-public class IllegalMediaTypeExceptionAdvice {
+public class MediaTypeExceptionAdvice {
 
-    @ExceptionHandler(IllegalMediaTypeException.class)
-    public ResponseEntity<ResponseBody> handeIllegalMediaTypeException(IllegalMediaTypeException ex) {
+    @ExceptionHandler({IllegalMediaTypeException.class, InvalidMediaTypeException.class})
+    public ResponseEntity<ResponseBody> handeIllegalMediaTypeException(RuntimeException ex) {
         ResponseBody responseBody = ResponseBody.builder()
                 .status(HttpStatus.BAD_REQUEST)
                 .message(ex.getMessage())
