9f9db737ad9390ce0a88655aaded9b133729b2aa
Ruslan Kostiuk <rsln.kostuk@gmail.com>
2023-05-06 16:23:48 +0300

    Add user avatar uploading and downloading

--- src/main/java/com/my/selfimprovement/controller/UsersController.java
+++ src/main/java/com/my/selfimprovement/controller/UsersController.java
@@ -7,18 +7,27 @@ import com.my.selfimprovement.dto.response.ResponseBody;
 import com.my.selfimprovement.dto.request.UserRegistrationRequest;
 import com.my.selfimprovement.entity.User;
 import com.my.selfimprovement.service.UserService;
+import com.my.selfimprovement.util.LoadedFile;
 import com.my.selfimprovement.util.validation.UserRegistrationRequestValidator;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.propertyeditors.StringTrimmerEditor;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
 import java.util.Map;
 
@@ -77,6 +86,32 @@ public class UsersController {
                 .data(Map.of("count", count))
                 .build();
         return ResponseEntity.ok(responseBody);
+    }
+
+    @PutMapping("/{id}/avatar")
+    public ResponseEntity<ResponseBody> uploadAvatar(@PathVariable("id") long userId, @RequestParam("file") MultipartFile file) {
+        userService.setUserAvatar(file, userId);
+        ResponseBody responseBody = ResponseBody.builder()
+                .status(HttpStatus.OK)
+                .message("Avatar successfully uploaded")
+                .build();
+        return ResponseEntity.ok().body(responseBody);
+    }
+
+    @GetMapping("/{id}/avatar")
+    public ResponseEntity<Resource> getAvatar(@PathVariable("id") long userId) throws IOException {
+        LoadedFile avatarLoadedFile = userService.getUserAvatar(userId);
+        Resource avatarResource = new ByteArrayResource(avatarLoadedFile.getBytes());
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.CONTENT_DISPOSITION, "inline");
+        return ResponseEntity.ok()
+                .headers(headers)
+                .contentType(getMediaType(avatarLoadedFile.getPath()))
+                .body(avatarResource);
+    }
+
+    private MediaType getMediaType(Path filePath) throws IOException {
+        return MediaType.parseMediaType(Files.probeContentType(filePath));
     }
 
 }
