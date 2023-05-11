b21176de4ee4c2b4ec67e45cce2781e1df24a64b
Ruslan Kostiuk <rsln.kostuk@gmail.com>
2023-05-11 18:59:02 +0300

    Add method security

--- src/main/java/com/my/selfimprovement/service/CategoryService.java
+++ src/main/java/com/my/selfimprovement/service/CategoryService.java
@@ -1,12 +1,14 @@
 package com.my.selfimprovement.service;
 
 import com.my.selfimprovement.entity.Category;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface CategoryService {
 
+    @PreAuthorize("hasAnyAuthority('ADMIN', 'ROOT')")
     Category create(String categoryName);
 
     /**
@@ -14,6 +16,7 @@ public interface CategoryService {
      * @param categoryId category to remove id
      * @throws com.my.selfimprovement.util.exception.CategoryNotFoundException if category with specified id not found
      */
+    @PreAuthorize("hasAnyAuthority('ADMIN', 'ROOT')")
     void remove(long categoryId);
 
     List<Category> getAll();
