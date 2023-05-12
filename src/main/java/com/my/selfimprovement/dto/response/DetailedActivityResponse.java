16bdaa62b0435238185cd4fa6d24a934f062ebf9
Ruslan Kostiuk <rsln.kostuk@gmail.com>
2023-05-12 14:54:23 +0300

    Create DetailedActivityResponse dto

---
+++ src/main/java/com/my/selfimprovement/dto/response/DetailedActivityResponse.java
@@ -0,0 +1,32 @@
+package com.my.selfimprovement.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.my.selfimprovement.dto.request.ActivityType;
+import com.my.selfimprovement.entity.RepetitiveActivity;
+
+import lombok.Data;
+
+import java.util.Set;
+
+/**
+ * Represents response for any activity type. Only fields belonging to specified {@code activityType}
+ * may be set, other fields must be null
+ */
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class DetailedActivityResponse {
+    private ActivityType activityType = ActivityType.REGULAR;
+
+    // Activity properties
+    private long id;
+    private String name;
+    private String description;
+    private int minutesRequired;
+    private long authorId;
+    private Set<Long> categoryIds;
+    private Set<Long> userIds;
+
+    // RegularActivity properties
+    private RepetitiveActivity.PeriodType periodType;
+    private Long timesPerPeriod;
+}
