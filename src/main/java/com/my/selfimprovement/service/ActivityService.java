152123d69eca99f729423f8fb2cf34b10885691e
Ruslan Kostiuk <rsln.kostuk@gmail.com>
2023-05-12 14:52:39 +0300

    Add ActivityService

---
+++ src/main/java/com/my/selfimprovement/service/ActivityService.java
@@ -0,0 +1,19 @@
+package com.my.selfimprovement.service;
+
+import com.my.selfimprovement.dto.request.NewActivityRequest;
+import com.my.selfimprovement.entity.Activity;
+
+public interface ActivityService {
+
+    /**
+     * Creates new activity
+     * @param activityRequest activity data
+     * @param authorId activity author id
+     * @return newly created activity
+     * @throws com.my.selfimprovement.util.exception.UserNotFoundException if user with specified id does not exist
+     * @throws com.my.selfimprovement.util.exception.CategoryNotFoundException if activityRequest contains
+     * category id that does not exist
+     */
+    Activity create(NewActivityRequest activityRequest, long authorId);
+
+}
