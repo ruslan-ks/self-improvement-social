43bb7b71313bc3af8b24427945473acdd0a7d34d
Ruslan Kostiuk <rsln.kostuk@gmail.com>
2023-05-19 01:09:47 +0300

    Remove redundant response message from CategoriesController#delete; replace response status with NO_CONTENT

--- src/main/java/com/my/selfimprovement/controller/CategoriesController.java
+++ src/main/java/com/my/selfimprovement/controller/CategoriesController.java
@@ -11,8 +11,6 @@ import com.my.selfimprovement.util.validation.NewCategoryValidator;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.MessageSource;
-import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -30,8 +28,6 @@ public class CategoriesController {
     private final CategoryMapper categoryMapper;
 
     private final NewCategoryValidator newCategoryValidator;
-
-    private final MessageSource messageSource;
 
     @GetMapping
     public ResponseEntity<ResponseBody> getAll() {
@@ -53,8 +49,7 @@ public class CategoriesController {
     @DeleteMapping("{categoryId}")
     public ResponseEntity<ResponseBody> delete(@PathVariable long categoryId) {
         categoryService.remove(categoryId);
-        String message = messageSource.getMessage("category.removed", null, LocaleContextHolder.getLocale());
-        return HttpUtils.ok(message);
+        return ResponseEntity.noContent().build();
     }
 
 }
