c22b5f90062a2d3140d77cb812e3bb4845e3cc2d
Ruslan Kostiuk <rsln.kostuk@gmail.com>
2023-05-20 19:53:28 +0300

    Make use of UIMessage in DataNotFoundExceptionAdvice

--- src/main/java/com/my/selfimprovement/controller/advice/DataNotFoundExceptionAdvice.java
+++ src/main/java/com/my/selfimprovement/controller/advice/DataNotFoundExceptionAdvice.java
@@ -1,18 +1,22 @@
 package com.my.selfimprovement.controller.advice;
 
 import com.my.selfimprovement.dto.response.ResponseBody;
-import com.my.selfimprovement.util.HttpUtils;
 import com.my.selfimprovement.util.exception.DataNotFoundException;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class DataNotFoundExceptionAdvice {
 
     @ExceptionHandler(DataNotFoundException.class)
-    public ResponseEntity<ResponseBody> handleDataNotFoundException(DataNotFoundException ex) {
-        return HttpUtils.notFound(ex.getMessage());
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseBody handleDataNotFoundException(DataNotFoundException ex) {
+        return ResponseBody.builder()
+                .status(HttpStatus.NOT_FOUND)
+                .developerMessage(ex.getMessage())
+                .build();
     }
 
 }
