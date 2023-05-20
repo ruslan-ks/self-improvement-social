98dbb4692a66af8de73837e549cf9feecfcc2fd3
Ruslan Kostiuk <rsln.kostuk@gmail.com>
2023-05-20 18:46:52 +0300

    Rename ClientMessage to UIMessage

--- src/main/java/com/my/selfimprovement/util/i18n/UIMessage.java
+++ src/main/java/com/my/selfimprovement/util/i18n/UIMessage.java
@@ -4,4 +4,4 @@ package com.my.selfimprovement.util.i18n;
  * Contains message code and default message, so the client app can either provide its own translation
  * or use the default one.
  */
-public record ClientMessage(String messageCode, String defaultMessage) {}
+public record UIMessage(String messageCode, String defaultMessage) {}
