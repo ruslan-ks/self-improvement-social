b2d6100b1c2b8c4803a06aee5e4deed26e2da5ce
Ruslan Kostiuk <rsln.kostuk@gmail.com>
2023-05-25 00:15:06 +0300

    Add UserActivityMapper#toDetailedUserActivityResponse

--- src/main/java/com/my/selfimprovement/dto/mapper/UserActivityMapper.java
+++ src/main/java/com/my/selfimprovement/dto/mapper/UserActivityMapper.java
@@ -1,8 +1,10 @@
 package com.my.selfimprovement.dto.mapper;
 
+import com.my.selfimprovement.dto.response.DetailedUserActivityResponse;
 import com.my.selfimprovement.dto.response.ShortUserActivityResponse;
 import com.my.selfimprovement.entity.UserActivity;
 
 public interface UserActivityMapper {
     ShortUserActivityResponse toShortUserActivityResponse(UserActivity userActivity);
+    DetailedUserActivityResponse toDetailedUserActivityResponse(UserActivity userActivity);
 }
