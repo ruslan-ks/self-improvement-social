d8658101802a9b2c691a03ca37a2fb50514c6208
Ruslan Kostiuk <rsln.kostuk@gmail.com>
2023-05-26 15:42:55 +0300

    Extract methods that work with user activities from UserController to UserActivityController

--- src/main/java/com/my/selfimprovement/controller/UserController.java
+++ src/main/java/com/my/selfimprovement/controller/UserController.java
@@ -1,14 +1,11 @@
 package com.my.selfimprovement.controller;
 
-import com.my.selfimprovement.dto.mapper.UserActivityMapper;
 import com.my.selfimprovement.dto.mapper.UserMapper;
 import com.my.selfimprovement.dto.request.UserUpdateRequest;
 import com.my.selfimprovement.dto.response.*;
 import com.my.selfimprovement.dto.request.UserRegistrationRequest;
 import com.my.selfimprovement.dto.response.ResponseBody;
 import com.my.selfimprovement.entity.User;
-import com.my.selfimprovement.entity.UserActivity;
-import com.my.selfimprovement.service.UserActivityService;
 import com.my.selfimprovement.service.UserService;
 import com.my.selfimprovement.service.token.JwtService;
 import com.my.selfimprovement.util.HttpUtils;
@@ -41,8 +38,6 @@ public class UserController {
 
     private final UserService userService;
 
-    private final UserActivityService userActivityService;
-
     private final JwtService jwtService;
 
     private final PasswordEncoder encoder;
@@ -50,8 +45,6 @@ public class UserController {
     private final UserRegistrationRequestValidator userRegistrationRequestValidator;
 
     private final UserMapper userMapper;
-
-    private final UserActivityMapper userActivityMapper;
 
     private final Translator translator;
 
@@ -152,7 +145,7 @@ public class UserController {
         return ResponseBody.ok("followings", followings);
     }
 
-    @Operation(summary = "Add currenly logged user to followers of user with id {userId}")
+    @Operation(summary = "Add currently logged user to followers of user with id {userId}")
     @PutMapping("/{userId}/followers")
     public ResponseEntity<ResponseBody> addFollower(@PathVariable long userId, @AuthenticationPrincipal Jwt jwt) {
         try {
@@ -175,55 +168,6 @@ public class UserController {
             return HttpUtils.notFound(ex.getMessage());
         }
         return ResponseEntity.noContent().build();
-    }
-
-    @Operation(summary = "Get user activities(activities user is going through)")
-    @GetMapping("/{userId}/activities")
-    public ResponseBody getUserActivities(@PathVariable long userId, Pageable pageable) {
-        List<ShortUserActivityResponse> userActivities = userActivityService.getPage(userId, pageable)
-                .map(userActivityMapper::toShortUserActivityResponse)
-                .toList();
-        return ResponseBody.ok("userActivities", userActivities);
-    }
-
-    @GetMapping("/{userId}/activities/count")
-    public ResponseBody getUserActivityCount(@PathVariable long userId) {
-        long count = userActivityService.count(userId);
-        return ResponseBody.ok("user-activity-count", count);
-    }
-
-    @PostMapping("/activities")
-    public ResponseEntity<ResponseBody> addUserActivity(@RequestParam("activityId") long activityId,
-                                                        @AuthenticationPrincipal Jwt jwt) {
-        try {
-            userActivityService.add(activityId, jwtService.getUserId(jwt));
-        } catch (IllegalStateException ex) {
-            log.warn("Failed to add user activity: " + ex.getMessage());
-            return HttpUtils.badRequest(ex.getMessage());
-        }
-        return ResponseEntity.ok().build();
-    }
-
-    @DeleteMapping("/activities/{activityId}")
-    public void deleteUserActivity(@PathVariable long activityId, @AuthenticationPrincipal Jwt jwt) {
-        userActivityService.delete(jwtService.getUserId(jwt), activityId);
-    }
-
-    @Operation(summary = "Get user activity details including completions")
-    @GetMapping("/{userId}/activities/{activityId}")
-    public ResponseBody getUserActivity(@PathVariable long userId, @PathVariable long activityId) {
-        UserActivity userActivity = userActivityService.get(userId, activityId);
-        DetailedUserActivityResponse response = userActivityMapper.toDetailedUserActivityResponse(userActivity);
-        return ResponseBody.ok("userActivity", response);
-    }
-
-    @Operation(summary = "Get user activities details including completions")
-    @GetMapping("/{userId}/activities/completions")
-    public ResponseBody getUserActivitiesCompletions(@PathVariable long userId, Pageable pageable) {
-        List<DetailedUserActivityResponse> userActivities = userActivityService.getPage(userId, pageable)
-                .map(userActivityMapper::toDetailedUserActivityResponse)
-                .toList();
-        return ResponseBody.ok("userActivities", userActivities);
     }
 
 }
