ee581dfabb3d0347bebc4089f897c4ba0bb147c0
Ruslan Kostiuk <rsln.kostuk@gmail.com>
2023-05-26 12:12:10 +0300

    Refactor LimitedCompletionsActivityValidator: remove redundant value range check; replace MethodSource with Translator

--- src/main/java/com/my/selfimprovement/util/validation/activity/LimitedCompletionsActivityValidator.java
+++ src/main/java/com/my/selfimprovement/util/validation/activity/LimitedCompletionsActivityValidator.java
@@ -1,9 +1,8 @@
 package com.my.selfimprovement.util.validation.activity;
 
 import com.my.selfimprovement.dto.request.NewActivityRequest;
+import com.my.selfimprovement.util.i18n.Translator;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.MessageSource;
-import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 
@@ -11,24 +10,16 @@ import org.springframework.validation.Errors;
 @RequiredArgsConstructor
 public class LimitedCompletionsActivityValidator implements NewSpecificTypeActivityRequestValidator {
 
-    private static final String LIMIT_FIELD_NAME = "completionsLimit";
-
     private static final String LIMIT_NOT_NULL = "valid.limitedCompletionsActivity.completionsLimit.notNull";
-    private static final String LIMIT_VALUE_RANGE = "valid.limitedCompletionsActivity.completionsLimit";
 
-    private final MessageSource messageSource;
+    private final Translator translator;
 
     @Override
     public void validate(NewActivityRequest request, Errors errors) {
         Integer completionsLimit = request.getCompletionsLimit();
         if (completionsLimit == null) {
-            String message = messageSource.getMessage(LIMIT_NOT_NULL, null, LocaleContextHolder.getLocale());
-            errors.rejectValue(LIMIT_FIELD_NAME, LIMIT_NOT_NULL, message);
-            return;
-        }
-        if (completionsLimit < 1) {
-            String message = messageSource.getMessage(LIMIT_VALUE_RANGE, null, LocaleContextHolder.getLocale());
-            errors.rejectValue(LIMIT_FIELD_NAME, LIMIT_VALUE_RANGE, message);
+            String message = translator.translate(LIMIT_NOT_NULL);
+            errors.rejectValue("completionsLimit", LIMIT_NOT_NULL, message);
         }
     }
 
