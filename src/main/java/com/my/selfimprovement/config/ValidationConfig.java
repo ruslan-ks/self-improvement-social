f731d2f699fc15e058947e5884a14b744bf4e859
Ruslan Kostiuk <rsln.kostuk@gmail.com>
2023-05-26 12:22:46 +0300

    Add PeriodicalLimitedCompletionsActivityValidator

--- src/main/java/com/my/selfimprovement/config/ValidationConfig.java
+++ src/main/java/com/my/selfimprovement/config/ValidationConfig.java
@@ -3,7 +3,7 @@ package com.my.selfimprovement.config;
 import com.my.selfimprovement.dto.request.ActivityType;
 import com.my.selfimprovement.util.validation.activity.LimitedCompletionsActivityValidator;
 import com.my.selfimprovement.util.validation.activity.NewSpecificTypeActivityRequestValidator;
-import org.springframework.context.MessageSource;
+import com.my.selfimprovement.util.validation.activity.PeriodicalLimitedCompletionsActivityValidator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,13 +13,13 @@ import java.util.Map;
 public class ValidationConfig {
 
     @Bean
-    public Map<ActivityType, NewSpecificTypeActivityRequestValidator> newActivityRequestValidatorMap(MessageSource ms) {
-        var limitedCompletionsActivityValidator = new LimitedCompletionsActivityValidator(ms);
-
-        // Use the same validator both for LIMITED_COMPLETIONS and PERIODICAL_LIMITED_COMPLETIONS,
-        // since PERIODICAL_LIMITED_COMPLETIONS represents a subtype and doesn't require new specific validation steps
-        return Map.of(ActivityType.LIMITED_COMPLETIONS, limitedCompletionsActivityValidator,
-                ActivityType.PERIODICAL_LIMITED_COMPLETIONS, limitedCompletionsActivityValidator);
+    public Map<ActivityType, NewSpecificTypeActivityRequestValidator> newActivityRequestValidatorMap(
+            LimitedCompletionsActivityValidator limitedCompletionsActivityValidator,
+            PeriodicalLimitedCompletionsActivityValidator periodicalLimitedCompletionsActivityValidator) {
+        return Map.of(ActivityType.LIMITED_COMPLETIONS,
+                limitedCompletionsActivityValidator,
+                ActivityType.PERIODICAL_LIMITED_COMPLETIONS,
+                periodicalLimitedCompletionsActivityValidator);
     }
 
 }
