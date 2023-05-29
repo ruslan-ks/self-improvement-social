07f2fe07f125b637c70af68c2a84da1982b77bf8
Ruslan Kostiuk <rsln.kostuk@gmail.com>
2023-05-29 19:42:13 +0300

    Add FilterCriteriaException

---
+++ src/main/java/com/my/selfimprovement/util/exception/FilterCriteriaException.java
@@ -0,0 +1,9 @@
+package com.my.selfimprovement.util.exception;
+
+public class FilterCriteriaException extends RuntimeException {
+
+    public FilterCriteriaException(String message) {
+        super(message);
+    }
+
+}
