70cd788316e803cecde933e0861f4cd166521942
Ruslan Kostiuk <rsln.kostuk@gmail.com>
2023-05-29 19:57:28 +0300

    Replace composition with inheritance in JpaActivityCriteriaDao

--- src/main/java/com/my/selfimprovement/repository/dao/JpaActivityCriteriaDao.java
+++ src/main/java/com/my/selfimprovement/repository/dao/JpaActivityCriteriaDao.java
@@ -1,23 +1,14 @@
 package com.my.selfimprovement.repository.dao;
 
 import com.my.selfimprovement.entity.Activity;
-import com.my.selfimprovement.repository.filter.EntityPageRequest;
-import com.my.selfimprovement.repository.filter.FilterCriteria;
-import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
+import com.my.selfimprovement.repository.filter.converter.ActivityCriteriaToPredicateConverter;
 import org.springframework.stereotype.Repository;
 
-import java.util.Collection;
-
 @Repository
-@RequiredArgsConstructor
-public class JpaActivityCriteriaDao implements CriteriaDao<Activity> {
+public class JpaActivityCriteriaDao extends ConjunctionJpaCriteriaDao<Activity> implements CriteriaDao<Activity> {
 
-    private final ConjunctionJpaCriteriaDao<Activity> activityConjunctionJpaCriteriaDao;
-
-    @Override
-    public Page<Activity> getPage(EntityPageRequest pageRequest, Collection<FilterCriteria> criteriaList) {
-        return activityConjunctionJpaCriteriaDao.getPage(pageRequest, criteriaList);
+    public JpaActivityCriteriaDao(ActivityCriteriaToPredicateConverter converter) {
+        super(Activity.class, converter);
     }
 
 }
