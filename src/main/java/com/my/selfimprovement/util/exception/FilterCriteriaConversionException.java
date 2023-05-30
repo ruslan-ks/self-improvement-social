adf906c607dc5dfbfa9a54c7f48743d8045f07e4
Ruslan Kostiuk <rsln.kostuk@gmail.com>
2023-05-30 14:01:59 +0300

    Add FilterCriteriaConversionException

---
+++ src/main/java/com/my/selfimprovement/util/exception/FilterCriteriaConversionException.java
@@ -0,0 +1,9 @@
+package com.my.selfimprovement.util.exception;
+
+public class FilterCriteriaConversionException extends FilterCriteriaException {
+
+    public FilterCriteriaConversionException(String message) {
+        super(message);
+    }
+
+}
