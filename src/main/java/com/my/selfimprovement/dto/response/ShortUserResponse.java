62db3a32a55e2aa676d52796e06a05228309e518
Ruslan Kostiuk <rsln.kostuk@gmail.com>
2023-05-30 19:02:50 +0300

    Add user status and role to ShortUserResponse

--- src/main/java/com/my/selfimprovement/dto/response/ShortUserResponse.java
+++ src/main/java/com/my/selfimprovement/dto/response/ShortUserResponse.java
@@ -1,20 +1,17 @@
 package com.my.selfimprovement.dto.response;
 
+import com.my.selfimprovement.entity.User;
 import lombok.Data;
 
 import java.util.Date;
 
 @Data
 public class ShortUserResponse {
-
     private long id;
-
     private String name;
-
     private String surname;
-
     private Date birthday;
-
     private long activityCount;
-
+    private User.Status status;
+    private User.Role role;
 }
